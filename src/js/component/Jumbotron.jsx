750c3821b8a9d3149897b44c12547eb81a7a54bd
Maria Julia Fernandez Balice (MJ) <136009647+Mjuliafb@users.noreply.github.com>
2023-12-22 20:15:59 +0000

    sixth commit button que hacer fixed

--- src/js/component/Jumbotron.jsx
+++ src/js/component/Jumbotron.jsx
@@ -6,7 +6,9 @@ export const Jumbotron = () => {
             <div className="container-fluid py-5">
                 <h1 className="display-5 fw-bold">Felices fiestas!</h1>
                 <p className="col-md-12 fs-4">La Navidad no solo marca el final del año, sino que encapsula el espíritu de compartir, celebrar <br></br> y cultivar la paciencia humana, convirtiéndola en una época verdaderamente exepcional.</p>
-                <button className="btn btn-primary btn-lg" type="button">¿Qué hacer?</button>
+                <a href="#cards" className="btn btn-primary btn-lg">
+                    ¿Qué hacer?
+                </a>
             </div>
         </div>
     )
